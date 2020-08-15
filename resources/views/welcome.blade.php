291ee91a6fd5cc227e91bf9f437278fefccd03aa
Benjamin Rios <brios@sisma.cl>
2020-08-15 14:05:25 -0400

    2do nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,24 @@
 
 @section('content')
     <div class="container">
-        <h1>PÁGINA PRINCIPAL</h1>
-
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">¿Quienes somos?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cursos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Planes Academicos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+        <main class="py-4">
+            <h1>PÁGINA PRINCIPAL</h1>
+        </main>
     </div>
 @endsection
