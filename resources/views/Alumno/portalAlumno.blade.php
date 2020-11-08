c38ded3cb5e48014c9d7da87ce3a92a2c23e69bd
Benjamin Rios <brios@sisma.cl>
2020-11-08 15:31:54 -0300

    jqmeter

--- resources/views/Alumno/portalAlumno.blade.php
+++ resources/views/Alumno/portalAlumno.blade.php
@@ -69,6 +69,9 @@ box-shadow: 5px 5px 30px 7px rgba(0,0,0,0.25), -5px -5px 30px 7px rgba(0,0,0,0.2
     flex-direction: column;
   }
 }
+.inner-therm span{
+  font-size: smaller!important;
+}
 </style>
 
 <div class="container-fluid px-0 bg-ce0  mb-5">
