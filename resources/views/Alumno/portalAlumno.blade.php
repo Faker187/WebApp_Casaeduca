28e797ba473d305d3638696cea9775e059af9d2b
Benjamin Rios <brios@sisma.cl>
2020-11-08 15:19:59 -0300

    jqmeter

--- resources/views/Alumno/portalAlumno.blade.php
+++ resources/views/Alumno/portalAlumno.blade.php
@@ -163,10 +163,10 @@ box-shadow: 5px 5px 30px 7px rgba(0,0,0,0.25), -5px -5px 30px 7px rgba(0,0,0,0.2
             var totalClasesAsignatura = {!! json_encode($asignatura->totalClasesAsignatura) !!}
             var cantidadClasesCompletadas = {!! json_encode($asignatura->cantidadClasesCompletadas) !!}
 
-          if (isNaN(totalClasesAsignatura) == 'false') {
+          if (isNaN(totalClasesAsignatura) == false) {
             totalClasesAsignatura = 0;
           }
-          if (isNaN(cantidadClasesCompletadas) == 'false') {
+          if (isNaN(cantidadClasesCompletadas) == false) {
             cantidadClasesCompletadas = 0;
           }
 
@@ -176,7 +176,6 @@ box-shadow: 5px 5px 30px 7px rgba(0,0,0,0.25), -5px -5px 30px 7px rgba(0,0,0,0.2
             meterOrientation:'horizontal',
             width:'75%',
             height:'50px',
-            displayTotal: true,
             barColor: '#4269B0'
           });
           $('.outer-therm').addClass('mx-auto');
