a6da2f8e976bd8cb0159d1496ef159425bcb30bf
Benjamin Rios <brios@sisma.cl>
2020-11-08 14:43:02 -0300

    casaeducav9

--- resources/views/asignatura2.blade.php
+++ resources/views/asignatura2.blade.php
@@ -107,11 +107,6 @@ a:hover,a:focus{
 .bg-light2{
     background-color: #f8f9fa!important;
 }
-@media only screen and (max-width: 575px) {
-  #wrapper.toggled #sidebar-wrapper {
-    margin-left: -9rem;
-  }
-}
 @media (min-width: 768px){
     #wrapper.toggled #sidebar-wrapper {
         margin-left: -15rem;
