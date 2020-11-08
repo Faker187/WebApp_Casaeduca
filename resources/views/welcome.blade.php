3a7b92d8215057ddd12762e24a802413b719da84
Benjamin Rios <brios@sisma.cl>
2020-11-08 10:53:52 -0300

    casaeducav9

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -137,8 +137,8 @@
     <!-- ====== plan academico ====== -->
             <div class="row justify-content-center mr-0">
                 <div class="col-sm-12 mx-auto p-5">
-                    <h3 class="h3-responsive mt-3 text-center orange-text">Plan Académico<img class="card-img" src="{{ asset('public/img/academico.svg') }}" style="width: 50px;margin-bottom: 1rem;" alt="sombrero"></h3>
-                    <p class="color-ce0" style="margin-top: 2%; font-size: 120%; text-align: center">
+                    <h2 class="h2-responsive mt-3 font-weight-bold text-center orange-text">Plan Académico<img class="card-img" src="{{ asset('public/img/academico.svg') }}" style="width: 50px;margin-bottom: 1rem;" alt="sombrero"></h2>
+                    <p class="color-ce0 font-weight-bold" style="margin-top: 2%; font-size: 120%; text-align: center">
                     {{$invitacionPlanAcademico}}
                     </p>
                     <a class="btn btn-lg bg-ce0 text-white font-weight-bold rounded-lg" href="{{ url('/Planacademico') }}">Ver más</a>
