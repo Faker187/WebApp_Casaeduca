b01abc7642780d6979354195f34bffff7e3ed0c8
Benjamin Rios <brios@sisma.cl>
2020-11-09 19:37:54 -0300

    casaeducavf

--- resources/views/asignatura2.blade.php
+++ resources/views/asignatura2.blade.php
@@ -112,11 +112,7 @@ a:hover,a:focus{
         margin-left: -37rem;
     }
 }
-@media (max-width: 575px){
-    #wrapper.toggled #sidebar-wrapper {
-        margin-left: -19rem;
-    }
-}
+
 </style>
 
 {{-- {{dd($unidades)}} --}}
