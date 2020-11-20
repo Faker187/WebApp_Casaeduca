cf33a229886ca4739a31d7ecc66ba44830b60b35
Benjamin Rios <brios@sisma.cl>
2020-11-20 18:48:26 -0300

    caseducav11

--- app/Http/Controllers/SuscripcionController.php
+++ app/Http/Controllers/SuscripcionController.php
@@ -212,9 +212,10 @@ class SuscripcionController extends Controller
          
           
             // se realiza el pago y parte desde hoy
-            $fechaActual = date("Y-m-d");
+            $fechaActual =  date('Y-m-d H:i:s',strtotime('-3 hour',strtotime($fechaActual)));
             // y termina en la cantidad de meses seleccionada
             $fin_plan = date('Y-m-d', strtotime("+".$meses." months", strtotime($fechaActual)));
+            dd($fechaActual, $fin_plan);
       
           
             $alumno = new Alumno;
