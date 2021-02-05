f3a0a1bd09473f9ed44b336e1f8f8877ae419301
Benjamin Rios <benjka.17@gmail.com>
2021-02-05 12:53:00 -0300

    editor de textoclear

--- app/Http/Controllers/SuscripcionController.php
+++ app/Http/Controllers/SuscripcionController.php
@@ -110,8 +110,8 @@ class SuscripcionController extends Controller
                     $msj2->to("contacto@casaeduca.cl");
                 });
                 $emailto = $request->email;
-                Mail::send('emails.bienvenido', $data2, function($message) use ($emailto){
-                    $message->from('contacto@casaeduca.cl', 'Casa educa')->subject('Bienvenido a Casa educa')->to($emailto);
+                Mail::to($emailto)->send('emails.bienvenido', $data2, function($message){
+                    $message->from('contacto@casaeduca.cl', 'Casa educa')->subject('Bienvenido a Casa educa');
                 });
 
 
